9dc507d8faa2e7ad5a746204fd9a314bc2dbe7ad
Abraham Alvarado <abrahamalvarado@devarana.mx>
2022-09-29 11:25:54 -0500

    Test Video 2

--- pages/index.jsx
+++ pages/index.jsx
@@ -64,15 +64,15 @@ export default function Home() {
                 placeholder={"blur"}
             /> */}
 
-            <iframe className="w-full h-screen" src="https://www.youtube-nocookie.com/embed/UFQQjNegXLc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe className="w-full h-screen"  src="https://www.youtube-nocookie.com/embed/UFQQjNegXLc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
             <div className="absolute lg:left-28 left-10 bottom-12">
-                <div className="relative">
+                {/* <div className="relative">
                     <div className="border-0 border-l-4 px-4 h-[90%] w-1 absolute -left-5 top-3">
                     </div>
                     <h1 className="lg:text-7xl text-5xl text-white pb-5">¡Vive de lujo!</h1>
                     <p className="text-3xl text-white font-bold">Desde 4.8 mdp</p>
-                </div>
+                </div> */}
             <div className=" text-center mt-5 flex lg:hidden">
                 <button className="pink-button px-6">Agendar cita</button>
             </div>
