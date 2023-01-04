882b4af09a42170121f3eab33bf69a490575d7f1
Abraham Alvarado <abrahamalvarado@devarana.mx>
2023-01-04 11:11:25 -0600

    Optimización

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  optimizeFonts: true,
+    images: {
+        minimumCacheTTL: 2592000
+    },
+    headers: function () {
+        return [
+          {
+            source: '/:path*',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'public, max-age=31536000, immutable',
+              },
+            ],
+          },
+        ]
+      },
+      compress: true,
 }
 
 module.exports = nextConfig
