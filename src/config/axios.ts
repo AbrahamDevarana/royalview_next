f1ef85b79f4576c05ce9ae967554ac9d76d990ba
AbrahamDevarana <abrahamalvarado@devarana.mx>
2024-03-04 10:28:46 -0600

    Debug3

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 export const axiosClient = axios.create({
-    // baseURL: `${process.env.API_URL}/api`,
-    baseURL: 'https://develop-royalview-next.vercel.app/api',
+    baseURL: `${process.env.API_URL}/api`,
+    // baseURL: 'https://develop-royalview-next.vercel.app/api',
     
     headers: {
         'Content-Type': 'application/json',
