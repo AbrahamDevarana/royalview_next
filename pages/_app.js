8795f7f492ecec4461e410e79d32eedf8edb0e5b
AbrahamDevarana <abrahamalvarado@devarana.mx>
2024-03-14 13:44:13 -0600

    Pixel

--- pages/_app.js
+++ pages/_app.js
@@ -19,12 +19,36 @@ function MyApp({ Component, pageProps }) {
             })(window,document,'script','dataLayer','GTM-5HD662X');`
             }}          
     />
+
+	<Script id="facebook-pixel" strategy="afterInteractive"
+		dangerouslySetInnerHTML={{
+			__html: `!function(f,b,e,v,n,t,s)
+			{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+			n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+			if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+			n.queue=[];t=b.createElement(e);t.async=!0;
+			t.src=v;s=b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t,s)}(window, document,'script',
+			'https://connect.facebook.net/en_US/fbevents.js');
+			fbq('init', '238355449027861');
+			fbq('track', 'PageView');`
+			}}          
+	/>
+	
+
 	<Component {...pageProps} />
 	<noscript id="google-analytics"
 		dangerouslySetInnerHTML={{
 		__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5HD662X" height="0" width="0" style="display: none; visibility: hidden;" />`,
 		}}           
     />
+	<noscript id="facebook-pixel-noscript" strategy="afterInteractive"
+			dangerouslySetInnerHTML={{
+				__html: `<noscript><img height="1" width="1" style="display:none"
+				src="https://www.facebook.com/tr?id=238355449027861&ev=PageView&noscript=1"
+				/></noscript>`
+			}}
+		/>
     <Script id="TawkTo" strategy="afterInteractive"
             dangerouslySetInnerHTML={{
                 __html: `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
