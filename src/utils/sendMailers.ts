976a36b830ccb3c3c8afa924e231a0bc6601d577
AbrahamDevarana <abrahamalvarado@devarana.mx>
2024-03-15 16:07:51 -0600

    FB

--- src/utils/sendMailers.ts
+++ src/utils/sendMailers.ts
@@ -1,3 +1,5 @@
+import sha256 from "sha256";
+
 export const sendMail = async (form: any, token: string) => {
     const response = await fetch(`/api/mailer`, {
         method: "POST",
@@ -61,3 +63,39 @@ export const sendSalesforce = async (form :  any) => {
             console.log(error);
         });
 };
+
+
+export const sendFacebookApi = async (form: any) => {
+    const pixelVersion = process.env.NEXT_PUBLIC_FB_API_VERSION;
+    const pixelId = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
+    const pixelToken = process.env.NEXT_PUBLIC_FBACCESS_TOKEN;
+    const uri = `https://graph.facebook.com/${pixelVersion}/${pixelId}/events?access_token=${pixelToken}`;
+
+
+    const payload = {
+        event_name: "Lead",
+        event_time: Math.floor(Date.now() / 1000),
+        action_source: "website",
+        event_id: Date.now(),
+        event_source_url: window.location.href,
+        user_data: {
+            em: sha256(form.email),
+            ph: sha256(form.telefono),
+            fn: sha256(form.nombre),
+            client_user_agent: navigator.userAgent
+        },
+    };
+    await fetch(
+        uri,
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "data": [payload],
+                "test_event_code": "TEST9985"
+            }),
+        }
+    );
+}
